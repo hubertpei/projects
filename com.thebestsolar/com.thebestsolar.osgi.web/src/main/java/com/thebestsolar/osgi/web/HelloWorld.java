02ad786e3133be9c7b777fae2cea76b7ec8f2a3a
Hubert Pei <hubert.pei@sap.com>
2020-12-30 02:40:07 -0800

    osgi web and pom.xml build.xml

--- com.thebestsolar/com.thebestsolar.osgi.web/src/main/java/com/thebestsolar/osgi/web/HelloWorld.java
+++ com.thebestsolar/com.thebestsolar.osgi.web/src/main/java/com/thebestsolar/osgi/web/HelloWorld.java
@@ -6,14 +6,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorld extends HttpServlet {
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 	private static final long serialVersionUID = -8727030276526521384L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException {
 		PrintWriter writer = response.getWriter();
+		logger.info("Hello OSGI web application");
 		writer.append("Hello OSGI web application!");
 	}
 }
